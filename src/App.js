ab0766a8a49be8912fe2a7d5e5f522c09e3fa0e2
tushar725mittal <tushar725mittal@gmail.com>
2023-07-10 20:24:27 +0530

    original changes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import Blog from './pages/Blog/Blog';
 import Home from './pages/Home/Home';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
       <Footer />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Home/Hero.js
+++ src/pages/Home/Hero.js
@@ -13,7 +13,7 @@ export default function Hero() {
                     <p className="font-light mt-1 text-lg">
                         Get lost in the world of captivating travel stories and stunning destinations. Are you ready for an adventure?
                     </p>
-                    <a href="/blog" className="hover:">
+                    <a href="/blog" className="hover:bg-transparent">
                         <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">Dive In</button>
                     </a>
                 </div>
