222c88657848867541522b80b3274750240856a8
tushar725mittal <tushar725mittal@gmail.com>
2023-07-10 20:48:07 +0530

    carousel package corrected

--- src/pages/Home/RecentPosts.js
+++ src/pages/Home/RecentPosts.js
@@ -1,5 +1,5 @@
 import { Carousel } from "react-responsive-carousel";
-import "./carousel.min.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 export default function RecentPosts() {
     return (
         <div className="
