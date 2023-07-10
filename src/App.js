8f3b330378f3f27237a10a28cd18bd670ac7cfe6
tushar725mittal <tushar725mittal@gmail.com>
2023-07-10 18:20:38 +0530

    HashRouter added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 import Blog from './pages/Blog/Blog';
 import Home from './pages/Home/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
