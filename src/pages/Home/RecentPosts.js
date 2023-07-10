98d280978ed8d0d2344e171d958bf747e76fc861
tushar725mittal <tushar725mittal@gmail.com>
2023-07-10 20:38:57 +0530

    carousel css added

--- src/pages/Home/RecentPosts.js
+++ src/pages/Home/RecentPosts.js
@@ -1,5 +1,5 @@
 import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./carousel.min.css";
 export default function RecentPosts() {
     return (
         <div className="
